1076302b72df4585a777d744e838284093c2d4b4
josephbill <josephbill00@gmail.com>
2016-06-16 06:33:18 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+﻿# Contact-List-App-Mean-Stack
+﻿
+﻿Contact list app using the mean stack. This is based off michaelcheng429 YouTube tutorial series except instead of using mongojs I used mongoose. You can see michaelcheng429's YouTube series here https://www.youtube.com/watch?v=kHV7gOHvNdk&index=1&list=PLX2HoWE32I8Nkzw2TqcifObuhgJZz8a0U&nohtml5=False 
---
+++ server.js
@@ -0,0 +1,91 @@
+var express = require ('express');
+var app = express();
+var mongoose = require ('mongoose');
+var bodyParser = require('body-parser');
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost/contactlist');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {console.log("mongoose connection successful");});
+
+var Schema = mongoose.Schema;
+
+var contactlistSchema = new Schema({
+    name: String,
+    email: String,
+    number: String
+}, { versionKey: false, collection: 'contactlist'});
+
+var Contact = mongoose.model('Contact', contactlistSchema);
+
+app.get('/contactlist', function (req, res) {
+  console.log('I received a get request');
+    Contact.find({}, function (err, docs) {
+    	console.log(docs);
+        res.json(docs);
+    });
+});
+
+app.post('/contactlist', function (req, res) {
+	console.log(req.body);
+
+		Contact(req.body).save(function (err, data) {
+			if (err) {
+				console.log(err);
+			} else {
+				console.log('Saved : ', data ); 
+				res.json(data);
+			}
+		});
+});
+
+app.delete('/contactlist/:id', function(req, res){
+	var id = req.params.id;
+	console.log(id);
+
+	Contact.findByIdAndRemove(id, function(err, data) {
+    	if (err){
+    		res.send(err);
+    	} else {
+    		res.json(data);
+    	}
+	});
+});
+
+app.get('/contactlist/:id', function(req, res){
+	var id = req.params.id;
+	console.log(id);
+
+	Contact.findById(id, function(err, data) {
+    	if (err){
+    		res.send(err)
+    	} else {
+    		res.json(data);
+    	}
+	});
+});
+
+app.put('/contactlist/:id', function(req, res) {
+	var id = req.params.id;
+	console.log(req.body.name);
+
+	Contact.findByIdAndUpdate(id, 
+		{ $set: { name: req.body.name, 
+				  email: req.body.email, 
+				  number: req.body.number }
+				}, function (err, doc) {
+  		if (err) {
+  			res.send(err);
+  		} else {
+  			res.json(doc);
+  		}
+	});
+});
+
+app.listen(5000);
+
+console.log("Server is Running on port 5000");
